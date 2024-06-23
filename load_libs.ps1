e65869324f4a9b91d45641a093721c9af3e86ef4
zhazhalove <zhazhalove@gmail.com>
2024-06-23 18:01:17 -0400

    added deps

--- load_libs.ps1
+++ load_libs.ps1
@@ -22,7 +22,14 @@ $dllPaths = @(
     "$baseDir\System.Threading.Tasks.Extensions.4.5.4\lib\net461\System.Threading.Tasks.Extensions.dll",
     "$baseDir\Microsoft.Bcl.AsyncInterfaces.8.0.0\lib\net462\Microsoft.Bcl.AsyncInterfaces.dll",
     "$baseDir\System.Text.Json.8.0.3\lib\net462\System.Text.Json.dll",
+    "$baseDir\System.Formats.Asn1.8.0.0\lib\net462\System.Formats.Asn1.dll",
+    "$baseDir\System.Buffers.4.5.1\lib\net461\System.Buffers.dll",
+    "$baseDir\System.Numerics.Vectors.4.5.0\lib\net46\System.Numerics.Vectors.dll",
+    "$baseDir\System.Runtime.CompilerServices.Unsafe.6.0.0\lib\net461\System.Runtime.CompilerServices.Unsafe.dll",
+    "$baseDir\System.Text.Encodings.Web.8.0.0\lib\net462\System.Text.Encodings.Web.dll",
+    "$baseDir\System.ValueTuple.4.5.0\lib\net461\System.ValueTuple.dll",
     "$baseDir\Oracle.ManagedDataAccess.23.4.0\lib\net472\Oracle.ManagedDataAccess.dll"
+    #,"$baseDir\System.Diagnostics.DiagnosticSource.6.0.1\lib\net461\System.Diagnostics.DiagnosticSource.dll"
     # Add more paths as needed for other dependencies
 )
 
